6aae983a0d5f3532cb7e6d6f80960c7c232f6d15
Lasse Nybro Nielsen <lasso290391@hotmail.com>
2018-12-14 13:29:01 +0100

    Almost no changes.

---
+++ Chess/Models/IPiece.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.App_Start
+{
+    interface IPiece
+    {
+        //Properties
+        Game.Horz X { get; set; }
+        int Y { get; set; }
+        string Color { get; set; }
+        int ID { get; set; }
+        int GameID { get; set; }
+        int Moves { get; set; }
+        bool LastMove { get; set; }
+
+        //Methods
+        string Move(string move);
+    }
+}
---
+++ Chess/Models/Knight.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chess.App_Start
+{
+    public class Knight : IPiece
+    {
+        public Game.Horz X { get; set; }
+        public int Y { get; set; }
+        public string Color { get; set; }
+        public int ID { get; set; }
+        public int GameID { get; set; }
+        public int Moves { get; set; }
+        public bool LastMove { get; set; }
+
+        public string Move(string move)
+        {
+            char startX = move[0];
+            char startY = move[1];
+            char endX = move[2];
+            char endY = move[3];
+
+            if (startX == this.X && startY == this.Y)//Choosing the tile of the Piece
+            {
+                return string.Concat("K", move);
+            }
+            else { throw new Exception("You have not chosen this piece"); }
+        }
+    }
+}
