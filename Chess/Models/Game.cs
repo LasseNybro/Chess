8c38f667016fbdf80d46a90834ca474c7c346344
Peter Dam Lundgaard Sørensen <flaeskevom@msn.com>
2018-12-19 14:42:01 +0100

    Add GameController

--- Chess/Models/Game.cs
+++ Chess/Models/Game.cs
@@ -8,23 +8,26 @@ namespace Chess.Models
     public class Game
     {
         public int GameID { get; set; }
+        public string GameName { get; set; }
         public int WhiteID { get; set; }
         public int BlackID { get; set; }
         public DateTime StartTime { get; set; } 
         public string LastMove { get; set; }
         public string Moves { get; set; }
+        public static List<Piece> Pieces = new List<Piece>();
 
+        //Constructor
         public Game(int whiteID, int blackID)
         {
             GameID = 0; //temp 
             WhiteID = whiteID;
             BlackID = blackID;
             StartTime = DateTime.Now;
-            LastMove = 
+            LastMove = Moves;
         }
 
+     
 
-
-        public static List<Piece> Pieces = new List<Piece>();
+        
     }
 }
